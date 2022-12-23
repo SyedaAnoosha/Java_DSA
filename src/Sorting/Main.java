85c044dfb2dfd142f767ddcfd20cab8c17949d5e
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-24 02:03:12 +0500

    Initial commit

---
+++ src/Sorting/Main.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={10,6,8,3,2,7};
+        Sorting_Algos S = new Sorting_Algos();
+
+        S.Selection_Sort(arr);
+
+    }
+}
---
+++ src/Sorting/Sorting_Algos.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Sorting_Algos {
+    public void Selection_Sort(int[] arr){
+
+        //In every iteration the minimum number and if there exists any we replace it with the
+
+        int min,temp;
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            min=i;
+            for (int j = i+1; j < n ; j++) {
+                if(arr[j]<arr[min])
+                    min=j;
+            }
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    public void Bubble_sort(int[] arr){
+
+    }
+}
