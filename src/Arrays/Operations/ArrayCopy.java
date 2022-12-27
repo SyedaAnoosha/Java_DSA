9639c492c9a30cc488d5e88e1bb9f14b518f8701
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-27 21:07:30 +0500

    Commit

---
+++ src/Arrays/Operations/ArrayCopy.java
@@ -0,0 +1,12 @@
+package Arrays.Operations;
+
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {6,7,8,9,0};
+        System.arraycopy(arr1,0,arr2,0,2);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]+" ");
+        }
+    }
+}
---
+++ src/Arrays/Operations/Insertion.java
@@ -0,0 +1,4 @@
+package Arrays.Operations;
+
+public class Insertion {
+}
