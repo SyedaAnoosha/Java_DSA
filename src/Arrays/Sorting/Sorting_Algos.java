00bf193b9f5dd6c78295814428cff24ab681240b
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-29 22:59:23 +0500

    Commit

--- src/Arrays/Sorting/Sorting_Algos.java
+++ src/Arrays/Sorting/Sorting_Algos.java
@@ -6,7 +6,6 @@ public class Sorting_Algos {
     public void Selection_Sort(int[] arr){
 
         //In every iteration the minimum number and if there exists any we replace it with the
-
         int min,temp;
         int n = arr.length;
         for (int i = 0; i < n-1; i++) {
@@ -20,8 +19,6 @@ public class Sorting_Algos {
             arr[min] = temp;
         }
         System.out.println(Arrays.toString(arr));
-
-
     }
 
     public void Bubble_sort(int[] arr){
