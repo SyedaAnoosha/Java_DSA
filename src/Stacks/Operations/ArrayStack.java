e8c736b5cc55d14b6e3584e0d7510007783170aa
SyedaAnoosha <anushazaidi06@gmail.com>
2023-01-21 01:14:33 +0500

    Commit

--- src/Stacks/Operations/ArrayStack.java
+++ src/Stacks/Operations/ArrayStack.java
@@ -1,6 +1,5 @@
 package Stacks.Operations;
-
-import java.util.EmptyStackException;
+import java.util.*;
 
 public class ArrayStack implements Stack{
     private int size = 0;
@@ -11,30 +10,11 @@ public class ArrayStack implements Stack{
         a = new Object[cap];
     }
 
-    public Object peek() {
-        if(size == 0){
-            throw new EmptyStackException();
+    public void push(Object obj) {
+        if (size == a.length) {
+            resize();
         }
-        return a[size-1];
-    }
-    public String toString() {
-        StringBuilder s = new StringBuilder("[");
-        for (int i = 0; i < size-1; i++) {
-            s.append(a[i]).append(", ");
-        }
-        s.append(a[size-1] +"]");
-        System.out.println(s);
-        return "";
-    }
-
-    public Object popFromLast(){
-        Object obj = a[0];
-        a[0] = null;
-        for (int i = 0; i < size-1; i++) {
-            a[i]=a[i+1];
-        }
-        size--;
-        return obj;
+        a[size++] = obj;
     }
 
     public Object pop() {
@@ -46,18 +26,13 @@ public class ArrayStack implements Stack{
         return obj;
     }
 
-    public void push(Object obj) {
-        if (size == a.length) {
-            resize();
+    public Object peek() {
+        if(size == 0){
+            throw new EmptyStackException();
         }
-        a[size++] = obj;
+        return a[size-1];
     }
 
-    private void resize(){
-        Object[] newArr = a;
-        a = new Object[ 2 * newArr.length];
-        System.arraycopy(newArr,0, a,0, size);
-    }
     public Object size() {
         return size;
     }
@@ -68,4 +43,89 @@ public class ArrayStack implements Stack{
         }
         return false;
     }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder("[");
+        int i =0;
+        while(i<size){
+            if( i == size-1 ){
+                s.append(a[i]);
+                break;
+            }
+            s.append(a[i]).append(", ");
+            i++;
+        }
+        s.append("]");
+        return s.toString();
+    }
+
+    public Object peekSecondLast() {
+        if (size < 2) {
+            throw new NoSuchElementException();
+        }
+        return a[size - 2];
+    }
+
+    public Object popSecondLast(){
+        if(size<2){
+            throw new NoSuchElementException();
+        }
+        Object obj = a[size-2];
+        a[size-2] = a[size-1];
+        a[size-1] = null;
+        size--;
+        return obj;
+
+    }
+
+    public Object popBottom(){
+
+        if (size == 0) {
+            throw new java.util.EmptyStackException();
+        }
+
+        Object obj = a[0];
+        a[0] = null;
+        for (int i = 0; i < size-1; i++) {
+            a[i]=a[i+1];
+        }
+        size--;
+        return obj;
+    }
+
+    public Object bottom() {
+        if (size == 0) {
+            throw new java.util.EmptyStackException();
+        }
+        return a[0];
+    }
+
+
+    private void resize(){
+        Object[] newArr = a;
+        a = new Object[ 2 * newArr.length];
+        System.arraycopy(newArr,0, a,0, size);
+    }
+
+    public boolean equals(ArrayStack stack){
+        if(this.size != stack.size) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if(this.a[i] != stack.a[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void reverse() {
+        for (int i = 0; i < size / 2; i++) {
+            Object e = a[i];
+            a[i] = a[size - 1 - i];
+            a[size - 1 - i] = e;
+        }
+
+    }
+
 }
--- src/Stacks/Operations/StackPractice.java
+++ src/Stacks/Operations/StackPractice.java
@@ -1,5 +1,7 @@
 package Stacks.Operations;
 
+import java.util.Arrays;
+
 public class StackPractice {
     public static void main(String[] args) {
 
@@ -9,9 +11,26 @@ public class StackPractice {
         stack.push(3);
         stack.push(4);
         stack.push(5);
-        System.out.println("Size: " + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        stack.popBottom();
+        System.out.println(stack);
+      /*System.out.println("Size: " + stack.size());
         System.out.println("Topmost: " + stack.peek());
+        System.out.println("Pop: "+ stack.pop());
+        stack.popSecondLast();
+        System.out.println(stack);
+        System.out.println(stack.peekSecondLast());
 
-
+        System.out.println();
+        ArrayStack stack2 = new ArrayStack(5);
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        stack2.push(4);
+        stack2.push(5);
+        stack2.reverse();
+        System.out.println(stack2);
+        System.out.println(stack.equals(stack2));*/
     }
 }
