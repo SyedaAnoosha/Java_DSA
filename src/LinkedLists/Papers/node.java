03f6497f0fa1a0d2f9ed0ac6786de6399cb152f8
SyedaAnoosha <anushazaidi06@gmail.com>
2023-01-22 17:13:58 +0500

    Commit

---
+++ src/LinkedLists/Papers/node.java
@@ -0,0 +1,14 @@
+package LinkedLists.Papers;
+
+public class node {
+    String data;
+    node next;
+
+    public node() {
+    }
+
+    public node(String data) {
+        this.data = data;
+        this.next=null;
+    }
+}
