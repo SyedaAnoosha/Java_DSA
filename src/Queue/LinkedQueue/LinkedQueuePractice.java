01753c841b18510e16d89b825615af5229506416
SyedaAnoosha <anushazaidi06@gmail.com>
2023-03-30 20:35:09 +0500

    Committed

--- src/Queue/LinkedQueue/LinkedQueuePractice.java
+++ src/Queue/LinkedQueue/LinkedQueuePractice.java
@@ -8,9 +8,15 @@ public class LinkedQueuePractice {
         L.add("third");
         L.add("fourth");
         L.add("fifth");
+        System.out.println(L);
+        System.out.println(L.getMiddle());
+        L.reverse();
+        System.out.println(L);
 
-        System.out.println(L.first());
-        System.out.println(L.peekSecondLast());
-        System.out.println(L.peekSecond());
+//        System.out.println(L.first());
+//        System.out.println(L.peekSecondLast());
+//        System.out.println(L.peekSecond());
+//        System.out.println(L);
+//        L.toArray();
     }
 }
--- src/Stacks/LinkedStack/LinkedStack.java
+++ src/Stacks/LinkedStack/LinkedStack.java
@@ -128,4 +128,5 @@ public class LinkedStack implements Stack {
         }
         return true;
     }
+
 }
